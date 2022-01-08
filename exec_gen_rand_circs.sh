cec9f278fdd66bc8a4e1f6d5160af00d7750426f
alvinquantum <agonzales@anl.gov>
2022-01-08 03:53:45 -0600

    Test

--- exec_gen_rand_circs.sh
+++ exec_gen_rand_circs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # For submitting multiple jobs of the gen_rand_circs.sh
-for idx in 1 5 10 15 20 25 30 35 40 
+for idx in 80 #1 5 10 15 20 25 30 35 40 
 do
     # number of qubits, depth, number of circuits
     sbatch gen_rand_circs.sh 10 $idx 50
