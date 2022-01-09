376c8fedc914bd68eb33c546c74a31d59eb94f42
alvinquantum <agonzales@anl.gov>
2022-01-08 23:18:31 -0600

    Refactor
    Pushed most of the functionality from test_circuits_circ.py into circtester.py. I also encapsulated the code in circtester.py into classes.

--- test_circuits_cirq.py
+++ test_circuits_cirq.py
@@ -3,18 +3,17 @@
 import os 
 from copy import deepcopy
 import numpy as np
-import pickle 
 import circtester 
-import json 
 import psutil
+import pickle
 import time
 from multiprocessing import Pool, Value, Array, Manager
 from cirq.contrib.qasm_import import circuit_from_qasm
-from cirq.qis import fidelity
 
 if __name__ == "__main__":
+    time0=time.time()
     print("running...")
-    #Program parameters. These are globals in the parallel program passed from shell.
+    #Program parameters.
     NUMBER_OF_QUBITS=5#int(sys.argv[1])
     CNOT_COUNT=10#int(sys.argv[2])
     START_CIRC_NUMBER=0#int(sys.argv[3])
@@ -37,63 +36,30 @@ if __name__ == "__main__":
     #     2.51188643e-03, 3.54813389e-03, 5.01187234e-03, 7.07945784e-03,
     #     1.00000000e-02, 0.015, 0.02, 0.025, 0.03, 0.035, 0.06]
     # Gets the files that match the string. Files include the path
-    rand_circ_files, circ_properties_files=circtester.get_files(
-        BASE_PATH, NUMBER_OF_QUBITS, CNOT_COUNT, 
-        START_CIRC_NUMBER, END_CIRC_NUMBER)
+    files_manipulator=circtester.FilesManipulator(BASE_PATH, NUMBER_OF_QUBITS, CNOT_COUNT)
+    circ_files, circ_properties_files=files_manipulator.get_files(START_CIRC_NUMBER, END_CIRC_NUMBER)
 
     #Get the qasm and pickle info
-    for idx, file_name in enumerate(circ_properties_files):
+    for file_idx, file_name in enumerate(circ_files):
         #Stopwatch
-        time0=time.time()
-        # os.environ['QISKIT_IN_PARALLEL'] = 'FALSE'
-
-        #get the file information of the circuit. Len 3 list: qubits#, cnot#, circuit_number#.
-        file_info=[int(elem) for elem in file_name.split("_") if elem.isdigit()]
-        assert len(file_info) == 3, "This file doesn't have three numbers in the name."
-        file_number=file_info[2]
+        time1=time.time()
         #If the file exists we already did this so just skip. Later on we can remove this for other initial states.
-        if circtester.result_exists(BASE_PATH, file_name, 0):
-            continue
-        
-        checks_properties=circtester.get_checks_properties(BASE_PATH, file_name)
-        circ_pieces=circtester.split_circuit_by_barrier(os.path.join(BASE_PATH, rand_circ_files[idx]))
-        if len(circ_pieces)!=3:
-            continue
-        # If there were no Pauli checks found just skip.
-        if checks_properties.p2_weights == 0: 
+        if files_manipulator.result_exists(file_name):
             continue
 
-        created_circs=circtester.create_circs(NUMBER_OF_QUBITS, circ_pieces)
-        # print(created_circs)
-        cirq_circ_with_checks =created_circs.cirq_circ_with_checks
-        cirq_circ_no_checks=created_circs.cirq_circ_no_checks
-        # cirq_circ_initial_state=created_circs.circ_initial_state
-        # print(cirq_circ_initial_state)
+        circuit_maker=circtester.CircuitMaker(os.path.join(BASE_PATH, file_name), NUMBER_OF_QUBITS)
+        noiseless_circs=circuit_maker.make_noiseless_circs()
 
         keep_qubits=list(range(NUMBER_OF_QUBITS))
+        circ_tester=circtester.CircuitTester(noiseless_circs, NUMBER_OF_QUBITS, keep_qubits)
+        assert circ_tester.sanity_check_fidelity, f"Sanity check fidelity failed for circuit {file_name}"
 
-        rho_correct=circtester.get_result_rho(cirq_circ_no_checks, NUMBER_OF_QUBITS, keep_qubits)
-        rho_checks=circtester.get_result_rho(cirq_circ_with_checks, NUMBER_OF_QUBITS+1, keep_qubits)
-        sanity_check_fidelity=fidelity(rho_checks, rho_correct, qid_shape=(2**(NUMBER_OF_QUBITS),), validate=False)
-        print(f"Sanity check fidelity: {sanity_check_fidelity}")
-        assert sanity_check_fidelity>0.98, f"Sanity check fidelity failed for circuit {file_name}"
-
-        #Noisy stuff.
-        cirq_circ_compute=circuit_from_qasm(circ_pieces[1].qasm())
         results=[]
-        circ_tester=circtester.CircuitTester(
-            cirq_circ_with_checks, cirq_circ_no_checks, NUMBER_OF_QUBITS, 
-            rho_correct, sanity_check_fidelity, keep_qubits)
         for error_idx, single_qubit_error in enumerate(SINGLE_QUBIT_ERROR_SPACE):
             results.append(circ_tester.run_test((error_idx, single_qubit_error)))
 
-        with open(os.path.join(BASE_PATH, file_name), "rb") as circ_file:
-            circ_info=pickle.load(circ_file)
-        circtester.store_results(
-            cirq_circ_compute, created_circs.cirq_circ_with_checks, BASE_PATH, file_name, 
-            NUMBER_OF_QUBITS, results, CNOT_COUNT, circ_info["rz"], circ_info["circuit_num"], 
-            circ_info["found_matches"], circ_info["max_pauli_weight"], circ_info["max_pauli_str_p1"], 
-            circ_info["max_pauli_str_p2"])
+        files_manipulator.store_results(circ_properties_files[file_idx], noiseless_circs, results)
 
-        print(f"execution time {time.time()-time0}")
+        print(f"file execution time {time.time()-time1}")
+    print(f"total execution time {time.time()-time0}")
     print("Finished.")
